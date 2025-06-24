3b1bfa15d4a44d485e5109fb317b2dd404a8990f
Briamco <crossinerobriamco@gmail.com>
2025-06-23 23:26:50 -0400

    Initialize new C# project structure for Ruleta with main program and project files

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using Ruleta.Data;
+using Ruleta.Screen;
+
+internal class Program
+{
+  private static void Main(string[] args)
+  {
+    EstudiantesData.CargarEstudiantes();
+
+    Screen.ScreenMain();
+  }
+}
